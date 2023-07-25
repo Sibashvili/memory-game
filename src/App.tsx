f8f90f8e93c6ad99115aa79f07846c63013598bf
Giorgi Sibashvili <124514809+Sib063@users.noreply.github.com>
2023-07-25 15:54:00 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+
+import "./App.css";
+
+import startPage from "./Components/startPage";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
---
+++ src/Components/startPage.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+export default App;
+export default function startPage() {
+  <Body>
+    <HeadTitle>memory</HeadTitle>
+  </Body>;
+}
+const Body = styled.section`
+  width: 100vw;
+  height: 100vh;
+`;
+const HeadTitle = styled.a`
+  font-size: 32px;
+  color: #fcfcfc;
+`;
